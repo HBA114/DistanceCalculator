626918d262cdde91716c76a90ed351f31002edbc
Hasan Basri Ayhaner <hbasriayhaner114@gmail.com>
2023-07-28 10:40:52 +0300

    Changed distance calculation result due to Metric

--- Helpers/Calculator.cs
+++ Helpers/Calculator.cs
@@ -1,10 +1,11 @@
 using DistanceCalculator.Entities;
+using DistanceCalculator.Enums;
 
 namespace DistanceCalculator.Helpers;
 
 public static class Calculator
 {
-    public static double Calculate(Coordinate coordinate1, Coordinate coordinate2)
+    public static double Calculate(Coordinate coordinate1, Coordinate coordinate2, Metric metric = Metric.Meter)
     {
         var latitude1 = coordinate1.Latitude * (Math.PI / 180.0);
         var longitude1 = coordinate1.Longitude * (Math.PI / 180.0);
@@ -16,6 +17,12 @@ public static class Calculator
                     Math.Cos(latitude1) * Math.Cos(latitude2) * Math.Pow(Math.Sin(longitude2 / 2.0), 2.0);
 
         var result = 6376500.0 * (2.0 * Math.Atan2(Math.Sqrt(d3), Math.Sqrt(1.0 - d3)));
-        return result;
+        
+        return metric switch
+        {
+            Metric.Meter => result,
+            Metric.Kilometer => result / 1000,
+            _ => throw new Exception($"Metric not implemented: {metric}")
+        };
     }
 }
