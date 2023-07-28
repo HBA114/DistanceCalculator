df80ab2bc6e3160c50e97353b3ea0b3e28c1cef0
Hasan Basri Ayhaner <hbasriayhaner114@gmail.com>
2023-07-28 10:41:24 +0300

    Created sample for testing new method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using DistanceCalculator.Entities;
+using DistanceCalculator.Enums;
 using DistanceCalculator.Helpers;
 
 Coordinate coordinate1 = new(41.0853120, 28.9770658);
@@ -7,3 +8,7 @@ Coordinate coordinate2 = new(41.0558508, 29.1054691);
 var res = Calculator.Calculate(coordinate1, coordinate2);
 res = Math.Round(res, 2);
 Console.WriteLine($"Distance: {res} meters");
+
+var resKm = Calculator.Calculate(coordinate1, coordinate2, Metric.Kilometer);
+resKm = Math.Round(resKm, 2);
+Console.WriteLine($"Distance: {resKm} kilometers");
